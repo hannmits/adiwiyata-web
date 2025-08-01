9ad8a43c2d2e710fe9037ff2dd346b3c0b5bcf14
hannmits <hanr26805@gmail.com>
2025-08-01 09:09:27 +0700

    ayub menambahkan porject

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Welcome to My Adieiyata web</h1>
+      <p>This is a simple React application.</p>
+      <button onClick={() => alert('Button clicked!')}>Click Me</button>
+    </>
+  )
+}
+
+export default App
