b64f11e68314d2c92ff1ccbb5e8d01e2370ac6f2
charismaw <kharismanwar@gmail.com>
2025-08-01 10:40:06 +0700

    kharisma mencoba mengganti app

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,8 @@
-// import { useState } from 'react'
-import './App.css'
+import React from 'react'
 
 function App() {
-
   return (
-    <>
-<<<<<<< HEAD
-      <p>anjai mabar</p>
-      <button onClick={() => alert('Button clicked!')}>Click Me</button>
-=======
-     <p>mencoba untuk beteter </p>
->>>>>>> e97083126f020a13ae34998d28468ca920e70ca7
-    </>
+    <div>App</div>
   )
 }
 
